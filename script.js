6439598e4e046271e91a875df70c856c110c9772
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2023-01-20 18:40:01 -0300

    functionalities to open pop up working

---
+++ script.js
@@ -0,0 +1,12 @@
+function removeModal(){
+    let modal = document.getElementById("modal_id")
+    modal.classList.remove("open")
+    
+
+}
+
+function openModal(){
+    let modal = document.getElementById("modal_id")
+    modal.classList.add("open")
+    
+}
