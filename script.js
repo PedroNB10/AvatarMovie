3e0045046a8b8e12ebb0273e50eddd927fb996bf
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2023-01-22 19:15:46 -0300

    last modifications with video

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-function removeModal(){
-    let modal = document.getElementById("modal_id")
-    modal.classList.remove("open")
-    
+const video = document.getElementById("video");
 
-}
+const videoLink = document.getElementById("video").src
+
 
 function openModal(){
     let modal = document.getElementById("modal_id")
     modal.classList.add("open")
+    video.setAttribute("src", videoLink);
+    
     
 }
+
+function removeModal(){
+    let modal = document.getElementById("modal_id")
+    modal.classList.remove("open")
+  	video.setAttribute("src", "");
+
+
+}
+
